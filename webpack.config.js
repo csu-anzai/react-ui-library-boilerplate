3988c165f18b92e2a397ca3e7e726d2d69ffc76e
James Humphreys <james@binaryjim.co.uk>
2019-08-21 19:32:57 +0100

    Added scss and directory migration to Webpack build script

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,56 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+	entry: ['./src/styles/styles.scss'],
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'react-ui-library-boilerplate.js',
+		library: 'react-ui-library-boilerplate',
+		libraryTarget: 'umd'
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new CopyPlugin([
+      { from: './src/assets', to: 'assets' },
+      { from: './src/styles', to: 'styles' },
+    ]),
+	],
+	externals: {
+    react: 'react'
+  },
+	module: {
+		rules: [
+			{
+				test: /(\.jsx|\.js)$/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							exclude: /(node_modules|bower_components)/
+						}
+					}
+				]
+			},
+			{
+				test: /\.scss$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'styles/[name].min.css',
+						}
+					},
+					{
+						loader: 'postcss-loader'
+					},
+					{
+						loader: 'sass-loader'
+					}
+				]
+			}
+		]
+	}
+}
