78ab11fc8996c40238f215715d898a7e355e03e7
James Humphreys <jhumphreys@psecure.net>
2019-08-22 12:28:49 +0100

    Set mode from env flags on webpack config, dropped usage of MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,68 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const pkg = require('./package.json');
 
-module.exports = {
-	entry: ['./src/index.js'],
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'react-ui-library-boilerplate.js',
-		library: 'react-ui-library-boilerplate',
-		libraryTarget: 'umd'
-	},
-	plugins: [
-		new CleanWebpackPlugin(),
-		new CopyPlugin([
-      { from: './src/assets', to: 'assets' },
-      { from: './src/styles', to: 'styles' },
-		]),
-		new MiniCssExtractPlugin({
-			filename: 'styles/[name].min.css',
-		})
-	],
-	externals: {
-    react: 'react'
-  },
-	module: {
-		rules: [
-			{
-				test: /(\.jsx|\.js)$/,
-				use: [
-					{
-						loader: 'babel-loader',
-						options: {
-							exclude: /(node_modules|bower_components)/
-						}
-					}
-				]
-			},
-			{
-				test: /\.scss$/,
-				exclude: /node_modules/,
-				use: [
-					/*
-					{
-						loader: 'file-loader',
-						options: {
-							name: 'styles/[name].min.css',
-						}
-					},
-					*/
-					{
-						loader: MiniCssExtractPlugin.loader
-					},
-					'css-loader',
-					'postcss-loader',
-					'sass-loader'
-				]
-			}
-		]
-	}
-}
+module.exports = env => {
+
+  const libraryName = pkg.name;
+  let outputFile, mode;
+
+  if( env && env.development ) {
+    mode = 'development';
+    outputFile = `${libraryName}.js`;
+  }
+  else {
+    mode = 'production';
+    outputFile = `${libraryName}.min.js`;
+  }
+
+  return {
+    mode: mode,
+    entry: [
+      './src/index.js', 
+      './src/styles/styles.scss'
+    ],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: outputFile,
+      library: libraryName,
+      libraryTarget: 'umd'
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new CopyPlugin([
+        { from: './src/assets', to: 'assets' },
+        { from: './src/styles', to: 'styles' },
+      ])
+    ],
+    externals: {
+      react: 'react'
+    },
+    module: {
+      rules: [
+        {
+          test: /(\.jsx|\.js)$/,
+          exclude: /(node_modules|bower_components)/,
+          use: [
+            'babel-loader',
+          ]
+        },
+        {
+          test: /\.scss$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: mode === 'production' ? 'styles/[name].min.css' : 'styles/[name].css'
+              }
+            },
+            'postcss-loader',
+            'sass-loader'
+          ]
+        }
+      ]
+    }
+  };
+};
