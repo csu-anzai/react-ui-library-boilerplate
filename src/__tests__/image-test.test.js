095ccb4effc0c801f12dea0dcd5244464fca58d2
James Humphreys <jhumphreys@psecure.net>
2019-08-30 23:57:07 +0100

    Added unit test for imageTest component

---
+++ src/__tests__/image-test.test.js
@@ -0,0 +1,13 @@
+import ImageTest from '../js/components/image-test';
+
+describe('ImageTest', () => {
+
+	it('should render without crashing', () => {
+
+		shallow(
+			<ImageTest />
+    );
+    
+  });
+
+});
