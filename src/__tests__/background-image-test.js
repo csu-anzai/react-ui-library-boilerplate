d89a29d22625998865d357759255ebed2f6a4622
James Humphreys <jhumphreys@psecure.net>
2019-08-30 23:56:54 +0100

    Added unit test for backgroundImage component

---
+++ src/__tests__/background-image-test.js
@@ -0,0 +1,13 @@
+import BackgroundImageTest from '../js/components/background-image-test';
+
+describe('BackgroundImageTest', () => {
+
+	it('should render without crashing', () => {
+
+		shallow(
+			<BackgroundImageTest />
+    );
+    
+  });
+
+});
