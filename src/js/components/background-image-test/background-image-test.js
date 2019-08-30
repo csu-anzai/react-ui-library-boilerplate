86037d588a7a6b76d0584a49a6fbe971aaf1e1a2
James Humphreys <jhumphreys@psecure.net>
2019-08-30 13:34:34 +0100

    Added test background image component (JS, SCSS and story)

---
+++ src/js/components/background-image-test/background-image-test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BackgroundImageTest = props => {
+  return(
+    <p className="background-image-test">If you see an image, Webpack has been able to copy the SCSS background-image asset and resolve the URL</p>
+  );
+}
+
+export default BackgroundImageTest;
---
+++ src/stories/background-image-test.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import centered from '@storybook/addon-centered/react';
+import { BackgroundImageTest } from '../../dist/react-ui-library-boilerplate';
+
+storiesOf('BackgroundImageTest', module)
+  .addDecorator(centered)
+  .add('Default', () => 
+    <BackgroundImageTest />
+  );
