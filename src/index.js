86938e7b899c62501cdb3c07c33704f51581cf78
James Humphreys <jhumphreys@psecure.net>
2019-08-30 13:35:39 +0100

    Included new test components in build output

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-export { default as TestComponent } from './js/components/test-component';
+// --------------------------------------------------------
+// Project - React UI Library Boilerplate
+// Repository - https://github.com/BinaryJim/react-ui-library-boilerplate
+// Developer - James Humphreys
+// Developer URL - https://binaryjim.co.uk
+// --------------------------------------------------------
+
+export { default as Button } from './js/components/button';
+export { default as ImageTest } from './js/components/image-test';
+export { default as BackgroundImageTest } from './js/components/background-image-test';
