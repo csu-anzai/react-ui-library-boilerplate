61bbed639b042ace145a293cfc796d58c34428be
James Humphreys <jhumphreys@psecure.net>
2019-08-30 13:35:57 +0100

    Dropped old test component

--- src/stories/test-component.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-// import '../src/scss/storybook-styles.scss';
-
-import { TestComponent } from '../../dist/react-ui-library-boilerplate';
-
-storiesOf('Test Component', module)
-  .add('Default', () => <TestComponent />)
