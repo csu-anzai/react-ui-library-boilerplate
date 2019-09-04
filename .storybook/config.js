3b55445da06b98741276beffb34d010afbfb65c1
James Humphreys <jhumphreys@psecure.net>
2019-09-04 12:26:21 +0100

    #23: Set storybook to sort stories by "kind"

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,13 @@
-import { configure, addDecorator } from '@storybook/react';
+import { addParameters, configure, addDecorator } from '@storybook/react';
 import '../src/scss/storybook-styles.scss';
+
+addParameters(
+  {
+    options: {
+      sortStoriesByKind: true,
+    },
+  },
+);
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../src/stories', true, /\.stories\.js$/);
